5d0a61d8e84bb4210b3386962ef3094af21b2ec4
Radu Mircea <radumircea88@gmail.com>
2017-06-20 20:31:04 +0300

    Add files via upload

---
+++ frontend/tribute-page/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './components/app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent 
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
